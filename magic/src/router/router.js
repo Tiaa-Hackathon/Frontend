01c1bae32c600371f82aa79389e5a893b69d5114
Asad <asad200ana@gmail.com>
2023-05-04 22:48:46 +0530

    login/register (routers)

---
+++ magic/src/router/router.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Login from "../components/Login.vue";
+import Dashboard from "../components/Dashboard.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: Login,
+    },
+    // routing example for reference....
+    // {
+    //   path: "/dashboard",
+    //   name: "dashboard",
+    //   component: Dashboard,
+    //   props: {},
+    // },
+  ],
+});
