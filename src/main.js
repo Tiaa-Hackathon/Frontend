ec41ac44f1752feb912555bc2f40b1307afc9977
Achyut Shukla <achyut.shukla.btech2020@sitpune.edu.in>
2023-05-05 16:29:16 +0530

    [initial setup]: create post markdown editor added

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Vue from "vue";
+import VueQuill from "vue-quill-editor";
+import "quill/dist/quill.core.css"; // import styles
+import "quill/dist/quill.snow.css"; // for snow theme
+
 import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import "./assets/css/app.css";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueQuill);
+
 new Vue({
-  router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
