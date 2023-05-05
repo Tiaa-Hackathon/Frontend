5edc9e74fa99a1784cb7dd81dd52e571ce3f051f
Enziaki <58030340+enziaki@users.noreply.github.com>
2023-05-05 11:01:24 +0530

    Initial on crpost

---
+++ wysiwyg/src/App-depr.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="create-post-container">
+    <h1>Create Post</h1>
+    <div class="titleInput">
+      <input type="text" id="textInput" v-model="textInput" placeholder="Enter post Title" class="input"/>
+      <button @click="savetofile()">Save</button>
+      </div>
+    <wysiwyg v-model="content" class="editor"/>
+    <div class="content-display" v-html="content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: ""
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #2e3440;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1000px;
+  margin: 0;
+}
+
+
+.wysiwyg-editor{
+  min-height: 800px;
+  min-width: 600px;
+  font-size: 12;
+}
+
+.create-post-container {
+  background-color: #4c566a; 
+  padding: 20px;
+  margin: 20px;
+  text-align: left;
+  width: 70vw;
+  height: 70vh;
+  border-radius: 30px; 
+  box-shadow: #ECEFF4;
+  transition: box-shadow 0.3s ease-in-out;
+  color: #ECEFF4;
+}
+
+.create-post-container:hover{
+  box-shadow: 0 5px 15px #ECEFF4
+}
+.input{
+  width: 65vw;
+  padding: 10px;
+  border-radius: 15px;
+  background: #3b4252;
+  color: #ECEFF4;
+}
+
+h1 {
+  margin-top: 0;
+}
+
+.editor {
+  border: 1px solid #4c566a;
+  padding: 5px;
+  border-radius: 25px;
+
+}
+.editr--toolbar{
+  width: 70vw;
+  height: 70vh;
+  color: #3b4252;
+  border-radius: 10px;
+}
+
+.content-display {
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  max-width: 600px;
+  max-height: 600px;
+}
+</style>
---
+++ wysiwyg/src/App.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="create-post-container">
+    <h1>Create Post</h1>
+    <div class="titleInput">
+      <input type="text" id="textInput" v-model="textInput" placeholder="Enter post Title" class="input"/>
+      <button @click="savetofile()">Save</button>
+    </div>
+    <div class="editor-container">
+      <wysiwyg v-model="content" class="editor"/>
+      <div class="editor-footer">
+        <span class="editor-footer-text">Add image</span>
+        <span class="editor-footer-text">Add link</span>
+      </div>
+    </div>
+    <div class="content-display" v-html="content"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+      textInput: "",
+    };
+  },
+  methods: {
+    savetofile() {
+      // your save logic here
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #f1f1f1;
+}
+
+.create-post-container {
+  background-color: #fff; 
+  padding: 20px;
+  margin: 20px auto;
+  text-align: left;
+  max-width: 800px;
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+h1 {
+  margin-top: 0;
+  font-size: 24px;
+  color: #2e3d49;
+}
+
+.titleInput {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.input{
+  width: 100%;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background: #f1f1f1;
+  color: #2e3d49;
+  font-size: 18px;
+  font-weight: 500;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition: box-shadow 0.3s ease-in-out;
+}
+
+.input:focus {
+  outline: none;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+}
+
+button {
+  background-color: #2e3d49;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 20px;
+  font-size: 18px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #3c4c5a;
+}
+
+.editor-container {
+  margin-bottom: 20px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+.editor {
+  min-height: 400px;
+  padding: 10px;
+  font-size: 18px;
+  font-weight: 500;
+  color: #2e3d49;
+}
+
+.editor-footer {
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px;
+  border-top: 1px solid #ddd;
+}
+
+.editor-footer-text {
+  margin-left: 10px;
+  font-size: 16px;
+  font-weight: 500;
+  color: #282828;
+}
+</style>
