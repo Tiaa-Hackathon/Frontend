22060392fcbb1f3e8b5837f9d88068abd529154c
Achyut Shukla <achyut.shukla.btech2020@sitpune.edu.in>
2023-05-06 14:54:42 +0530

    [feature]: comment component added

---
+++ src/components/CommentsCard.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+  <div class="comment">
+    <h4>
+      by <a href="" style="color: black">{{ user.username }}</a> on
+      {{ created }}
+    </h4>
+    <p v-html="content"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "comment",
+  data() {
+    return {
+      user: {
+        username: "username",
+      },
+      created: "created",
+      content: "content",
+    };
+  },
+};
+</script>
+
+<style scoped lang="css">
+.comment {
+  background: #f2f2f2;
+  padding: 20px 10px;
+  margin-bottom: 20px;
+}
+
+h4 {
+  margin: 5px 0;
+}
+
+p {
+  margin: 5px 0;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,59 @@
 <template>
-  <div class="container">
-    <v-card class="mx-auto" max-width="400">
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      >
-        <v-card-title>Top 10 Australian beaches</v-card-title>
-      </v-img>
+  <!-- <v-card class="mx-auto" color="#26c6da" dark max-width="400">
+    <v-card-title>
+      <v-icon large left> mdi-twitter </v-icon>
+      <span class="text-h6 font-weight-light">Twitter</span>
+    </v-card-title>
 
-      <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+    <v-card-text class="text-h5 font-weight-bold">
+      "Turns out semicolon-less style is easier and safer in TS because most
+      gotcha edge cases are type invalid as well."
+    </v-card-text>
 
-      <v-card-text class="text--primary">
-        <div>Whitehaven Beach</div>
+    <v-card-actions>
+      <v-list-item class="grow">
+        <v-list-item-avatar color="grey darken-3">
+          <v-img
+            class="elevation-6"
+            alt=""
+            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+          ></v-img>
+        </v-list-item-avatar>
 
-        <div>Whitsunday Island, Whitsunday Islands</div>
-      </v-card-text>
+        <v-list-item-content>
+          <v-list-item-title>Evan You</v-list-item-title>
+        </v-list-item-content>
 
-      <v-card-actions>
-        <v-btn color="orange" text> Share </v-btn>
-
-        <v-btn color="orange" text> Explore </v-btn>
-      </v-card-actions>
-    </v-card>
-  </div>
+        <v-row align="center" justify="end">
+          <v-icon class="mr-1"> mdi-heart </v-icon>
+          <span class="subheading mr-2">256</span>
+          <span class="mr-1">·</span>
+          <v-icon class="mr-1"> mdi-share-variant </v-icon>
+          <span class="subheading">45</span>
+        </v-row>
+      </v-list-item>
+    </v-card-actions>
+  </v-card> -->
+  <Comment :comments="comments" />
 </template>
+
+<script>
+import Comment from "@/components/CommentsCard.vue";
+
+export default {
+  components: {
+    Comment,
+  },
+  data() {
+    return {
+      comments: [
+        {
+          name: "author name",
+          body: "comment",
+          email: "hello@gmail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
