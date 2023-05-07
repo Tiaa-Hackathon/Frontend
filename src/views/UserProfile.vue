18f92972c9cd554bf3ecef49f4d90ef7e2eaade8
zeeshaantandel <zeeshaan143tandel@gmail.com>
2023-05-08 03:50:42 +0530

    user profile card completed

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <v-row style="background-color: beige; height: 50vh; margin-top: 10vh; margin-left: 15vh; margin-right: 15vh;">
+    <v-row style="background-color: white; height: 40vh; margin-top: 10vh; margin-left: 15vh; margin-right: 15vh;">
+      <v-col cols="12">
       <v-row style="margin-top: 3vh;">
         <v-col cols="2">
-          <v-avatar size="20vh" style="margin-left: 3vh;">
+          <v-row>
+            <v-col>
+            <v-avatar size="20vh" style="margin-left: 3vh;">
             <v-img :src="userImg" class="rounded-full"></v-img> <!-- {{ UserImg }}-->
           </v-avatar>
+        </v-col>
+          </v-row>
+
+
+          
         </v-col>
         <v-col cols="1">
           <v-row style="margin-top: 1vh;">
@@ -19,7 +27,50 @@
             </v-col>
           </v-row>
         </v-col>
+        <v-col cols="1" offset="2">
+          <v-row style="margin-top: 2vh;">
+            <v-col>
+              <p class="number-post text-center">{{ userPosts }}</p>
+            </v-col>
+          </v-row>
+          <v-row style="margin-top: 1vh;">
+            <v-col>
+              <p class="post-text text-center">Posts</p>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="1" offset="1">
+          <v-row style="margin-top: 2vh;">
+            <v-col>
+              <p class="number-post text-center">{{ userUpvotes }}</p>
+            </v-col>
+          </v-row>
+          <v-row style="margin-top: 1vh;">
+            <v-col>
+              <p class="post-text text-center">Upvotes</p>
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="1" offset="1">
+          <v-row style="margin-top: 2vh;">
+            <v-col>
+              <p class="number-post text-center">{{ userDownvotes }}</p>
+            </v-col>
+          </v-row>
+          <v-row style="margin-top: 1vh;">
+            <v-col>
+              <p class="post-text text-center">Downvotes</p>
+            </v-col>
+          </v-row>
+        </v-col>
       </v-row>
+      <v-row>
+        <v-col>
+          
+              <p class="user-bio">{{ userBio }}</p>
+        </v-col>
+      </v-row>
+      </v-col>
     </v-row>
   </div>
 </template>
@@ -69,85 +120,6 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  max-width: 80vw;
-  margin: 0 auto;
-  padding: 24px;
-  border-radius: 10px;
-  margin-top: 5%;
-  background-color: #e5e5e5;
-}
-
-
-.profile-header {
-  display: flex;
-  align-items: center;
-  margin-bottom: 24px;
-}
-
-.profile-avatar {
-  margin-right: 24px;
-}
-
-.profile-name-container {
-  display: flex;
-  flex-direction: column;
-}
-
-.profile-name {
-  font-size: 36px;
-  font-weight: bold;
-  margin: 0;
-}
-
-.profile-title {
-  font-size: 24px;
-  font-weight: bold;
-  margin-top: 8px;
-  color: #0072b1;
-}
-
-.profile-location {
-  font-size: 18px;
-  margin-top: 8px;
-  color: #666666;
-}
-
-.profile-connections {
-  font-size: 18px;
-  margin-top: 8px;
-  color: #666666;
-}
-
-.user-bio {
-  font-size: 18px;
-  margin-bottom: 24px;
-  padding: 0.5rem;
-}
-
-.profile-stats {
-  display: flex;
-  justify-content: space-between;
-  border-top: 1px solid #e5e5e5;
-  padding: 24px;
-}
-
-
-.stat-item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.stat-number {
-  font-size: 36px;
-  font-weight: bold;
-}
-
-.stat-label {
-  font-size: 18px;
-  color: #666666;
-}
 
 .username-text{
   font-size: 175%;
@@ -159,5 +131,22 @@ export default {
 .email-text{
   color: #0072b1;
   font-weight: bold;
+}
+
+.number-post{
+  font-size: 36px;
+  font-weight: bold;
+}
+
+.post-text{
+  font-size: 18px;
+  color: #666666; 
+}
+
+.user-bio {
+  font-size: 18px;
+  margin-bottom: 24px;
+  margin-left: 15px;
+  padding: 0.5rem;
 }
 </style>
