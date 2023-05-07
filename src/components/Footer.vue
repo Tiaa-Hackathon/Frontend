2ebbbed214e34fe256d06ab1d41e2be67e3ba459
Enziaki <58030340+enziaki@users.noreply.github.com>
2023-05-07 22:08:45 +0530

    [Bug Fix]: Removed mdn-icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,19 +2,7 @@
     <v-footer color="black" style="opacity: 0.8;"  app>
       <v-row align="center" justify="center">
         <v-col md="6" sm="8" xs="12" class="text-center">
-          <v-btn icon href="#">
-            <v-icon style="color: white;">mdi-twitter</v-icon>
-          </v-btn>
-  
-          <v-btn icon href="#">
-            <v-icon style="color: white;">mdi-linkedin</v-icon>
-          </v-btn>
-  
-          <v-btn icon href="#">
-            <v-icon style="color: white;">mdi-github</v-icon>
-          </v-btn>
         </v-col>
-  
         <v-col md="6" sm="4" xs="12" class="text-center">
           <span class="white--text">&copy; All rights reserved.</span>
         </v-col>
