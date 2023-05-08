c94f99aece8d9205f0048de099e7298e9b7dec98
zeeshaantandel <zeeshaan143tandel@gmail.com>
2023-05-08 14:21:13 +0530

    footer changed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
-    <v-footer color="black" style="opacity: 0.8;"  app>
+    <v-footer color="black" style="opacity: 0.9; height: 7vh;"  app >
       <v-row align="center" justify="center">
-        <v-col md="6" sm="8" xs="12" class="text-center">
+        <v-col md="4" sm="8" xs="12" class="text-center">
+            <span class="white--text">ASQ</span>
         </v-col>
-        <v-col md="6" sm="4" xs="12" class="text-center">
+
+        <v-col md="4" sm="8" xs="12" class="text-center">
+            <span class="white--text">SIT Team A</span>
+        </v-col>
+
+        <v-col md="4" sm="4" xs="12" class="text-center">
           <span class="white--text">&copy; All rights reserved.</span>
         </v-col>
       </v-row>
